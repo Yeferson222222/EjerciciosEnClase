2ef8538fa0e80853b2224ad3c0a776db659adf3e
Yeferson222222 <162386600+Yeferson222222@users.noreply.github.com>
2024-03-06 22:36:03 -0500

    carga inicial

---
+++ TablasMultiplicar/src/tablasmultiplicar/TablasMultiplicar.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tablasmultiplicar;
+
+/**
+ *
+ * @author Yeferson
+ */
+public class TablasMultiplicar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
